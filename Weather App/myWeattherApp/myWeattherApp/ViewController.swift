7ccf6f0756ea1b0145d1befb371bc9eb424a4853
MRAO0212 <mayureshvrao@gmail.com>
2020-12-01 23:00:07 -0800

    show temperature

--- Weather App/myWeattherApp/myWeattherApp/ViewController.swift
+++ Weather App/myWeattherApp/myWeattherApp/ViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
-
+import Combine
 class ViewController: UIViewController {
 
+    @IBOutlet var temperatureLabel: UILabel!
+    
+    private var  webservice: Webservice = Webservice()
+    private var  cancellable: AnyCancellable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.cancellable = self.webservice.fetchweather(city: "Nagpur")
+            .catch{ _ in Just(Weather.placeHolder) }
+            .map {weather in
+                if let temp = weather.temp{
+                    return "\(temp)"
+                } else
+                {
+                    return "Error in getting weather!"
+                }
+            }.assign(to: \.text, on: self.temperatureLabel)
     }
 
 
