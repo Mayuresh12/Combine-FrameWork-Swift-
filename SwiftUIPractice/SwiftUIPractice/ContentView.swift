7871fefdb190f2857023c9e0ffa6593bbfedbdb5
MRAO0212 <mayureshvrao@gmail.com>
2020-12-02 23:09:45 -0800

    swift Ui practice

---
+++ SwiftUIPractice/SwiftUIPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPractice
+//
+//  Created by Mayuresh Rao on 12/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Mayuresh you are the best!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
